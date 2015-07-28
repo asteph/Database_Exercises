f800325991b98ae3d2825c507c48fb40dc720a05
asteph <alissa.stephens1@gmail.com>
2015-07-28 14:33:08 -0500

    in class example of creating a table

---
+++ users_migration.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	current_account_balance DECIMAL(15, 2),
+	phonenumber VARCHAR(13) NOT NULL,
+	member_since DATE NOT NULL,
+	UNIQUE(username),
+	PRIMARY KEY (id)
+);
