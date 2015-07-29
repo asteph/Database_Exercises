4372dcf345fb831c87e4964e869b786a3252006b
asteph <alissa.stephens1@gmail.com>
2015-07-29 10:49:57 -0500

    completed delete excercise; SELECT statements were converted to DELETE

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 
 SELECT '------------------------------' AS '';
 SELECT '------------------------------' AS 'Albums released after 1991';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 SELECT '------------------------------' AS '';
 SELECT '------------------------------' AS 'Disco genre';
-SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 SELECT '------------------------------' AS '';
 SELECT '------------------------------' AS 'Albums by Whitney Houston';
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
 
