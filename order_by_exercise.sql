dba786d3b216c4995f129ceb1e797869e04c0b1c
asteph <alissa.stephens1@gmail.com>
2015-07-29 14:56:13 -0500

    practice with ORDER BY using multiple conditionals

--- order_by_exercise.sql
+++ order_by_exercise.sql
@@ -6,13 +6,14 @@ WHERE (first_name = 'Irena'
 	OR first_name = 'Vidya'
 	OR first_name = 'Maya')
 	AND gender = 'M'
-ORDER BY first_name, last_name;
+ORDER BY last_name, first_name;
 
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT concat(first_name, ' ', last_name) AS full_name
+SELECT emp_no, concat(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE last_name LIKE 'E%'
-	OR last_name LIKE '%E';
+	OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
 	
 -- Find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT concat(first_name, ' ', last_name) AS full_name
@@ -23,10 +24,11 @@ WHERE last_name LIKE 'E%'
 
 -- Find all employees hired in the 90s and born on Christmas — 362 rows.
 
-SELECT first_name, last_name
+SELECT *
 FROM employees
 WHERE hire_date BETWEEN CAST('1990-01-01' AS DATE) AND CAST('1999-12-31' AS DATE)
-	AND birth_date LIKE '%-12-25';
+	AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 
 SELECT concat(first_name, ' ', last_name) AS full_name
 FROM employees
